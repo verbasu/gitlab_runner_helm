4756308ec2f12f534b85abc8d72ba968220116b3
Georgi N. Georgiev <ggeorgiev@gitlab.com>
2022-07-28 02:50:36 +0300

    fix message to search in integration tests to 'Starting multi-runner'

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -7,7 +7,7 @@ helm install -f values.yaml --timeout 5m --wait --set gitlabUrl="$CI_SERVER_URL"
 
 kubectl describe pod -l "$INTEGRATION_HELM_POD_RELEASE_LABEL"
 
-timeout 60s grep -m1 "Runner registered successfully" <(kubectl logs -f -l "$INTEGRATION_HELM_POD_RELEASE_LABEL")
+timeout 60s grep -m1 "Starting multi-runner" <(kubectl logs -f -l "$INTEGRATION_HELM_POD_RELEASE_LABEL")
 
 exit_code="$?"
 
